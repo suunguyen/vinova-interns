0a69a9219bdfc1f2fc23284cc5d9a30f4656805a
ryann126 <viincyy126@gmail.com>
2021-11-18 23:25:40 +0700

    Add some new Nodejs topics

---
+++ Research/HTTPServer.js
@@ -0,0 +1,57 @@
+// GET
+// const https = require('https')
+// const options = {
+//     hostname: 'example.com',
+//     port: 443,
+//     path: '/todos',
+//     method: 'GET'
+// }
+
+// const req = https.request(options, res => {
+//     console.log(`statusCode: ${res.statusCode}`)
+
+//     res.on('data', d => {
+//         process.stdout.write(d)
+//     })
+// })
+
+// req.on('error', error => {
+//     console.error(error)
+// })
+
+// req.end()
+
+// POST request
+const https = require('https')
+
+const data = new TextEncoder().encode(
+    JSON.stringify({
+        todo: 'Buy the milk 🍼'
+    })
+)
+
+const options = {
+    hostname: 'whatever.com',
+    port: 443,
+    path: '/todos',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    }
+}
+
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+        process.stdout.write(d)
+    })
+})
+
+req.on('error', error => {
+    console.error(error)
+})
+
+req.write(data)
+req.end()
