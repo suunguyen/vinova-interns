12732d77fb1e60e49cd3f0d5191083189867f377
ryann126 <viincyy126@gmail.com>
2021-12-12 15:37:56 +0700

    init base project

---
+++ base/module/Student/controllers/student.controller.ts
@@ -0,0 +1,8 @@
+import Student from '../models/student.model'
+import {Request, Response} from 'express'
+
+export default class StudentController{
+    async getAllStudents(req: Request, res: Response){
+        
+    }
+}
